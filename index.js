46c0d308db02150a07868bc8bbf6c99b629429f1
Brayzure <rictylp@gmail.com>
2021-05-07 13:10:04 -0700

    Fetch all users on startup, for methods that rely on members

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const Client = require("./src/Client.js");
 
 const { token } = require("./config/auth.json");
 
-const client = new Client(token);
+const client = new Client(token, {
+    getAllUsers: true
+});
 client.connect();
